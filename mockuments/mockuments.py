547e6d360c6af80ec3365fe0908d439f3cae69bb
mattcarabine <matt.carabine@hotmail.co.uk>
2015-12-25 00:15:55 +0000

    Improve throughput by persisting incrementally

--- mockuments/mockuments.py
+++ mockuments/mockuments.py
@@ -3,65 +3,63 @@ from couchbase.bucket import Bucket
 from templateloader import TemplateLoader
 from docgenerator import DocGenerator
 import multiprocessing
-import math
-import uuid
 import sys
+import time
 
-DEFAULT_BATCH_SIZE = 25
 DEFAULT_POOL_SIZE = 8
+DEFAULT_BATCH_SIZE = 1000
 
 
 def main():
     parse_arguments(sys.argv[1:])
-    template_loader = TemplateLoader('/Users/matt/Documents/test.json')
-    bkt = connect_to_cb()
-    generated_docs = generate_documents(template_loader.template)
-    store_in_cb(generated_docs, bkt)
+    template_loader = TemplateLoader(args.template)
+    generate_documents(template_loader.template)
 
 
 def generate_documents(template):
-    # TODO: Improve document generation, currently will use a lot of memory
-    # Unfortunately storage has to be serial, maybe use a lock?
-
-    manager = multiprocessing.Manager()
-    generated_docs = manager.dict()
+    # Potential issue is that the keys may collide, but this is unlikely
+    # with UUIDs
+    cb_queue = multiprocessing.Queue()
     processes = list()
+    start = time.time()
     for _ in xrange(0, DEFAULT_POOL_SIZE):
         p = multiprocessing.Process(target=per_process_doc_generator,
-                                    args=(template, generated_docs,))
+                                    args=(template, cb_queue,))
         p.start()
         processes.append(p)
-
+    p = multiprocessing.Process(target=store_in_cb, args=(cb_queue,))
+    p.start()
+    processes.append(p)
     for process in processes:
         process.join()
+    end = time.time()
+    time_elapsed = end - start
+    print('Took {} seconds to generate {} documents'.format(
+        time_elapsed, args.number_of_docs))
 
-    # Converts docs back to normal dict
-    generated_docs = {key: generated_docs[key]
-                      for key in generated_docs.keys()}
-    return generated_docs
 
-
-def per_process_doc_generator(template, generated_docs):
+def per_process_doc_generator(template, cb_queue):
     doc_generator = DocGenerator(template)
-    for _ in xrange(0, args.number_of_docs/DEFAULT_POOL_SIZE):
-        document = doc_generator.generate_document()
-        while document['key'] in generated_docs:
-            # Keep regenerating uuid until no collision
-            document['key'] = str(uuid.uuid4())
-        generated_docs[document['key']] = document['value']
+    start = time.time()
+    put = cb_queue.put
+    generate_document = doc_generator.generate_document
+    for i in xrange(0, args.number_of_docs/DEFAULT_POOL_SIZE):
+        document = generate_document()
+        put((document['key'], document['value']))
+    end = time.time()
+    print('docs generated in {} seconds'.format(end-start))
 
 
-def store_in_cb(documents, bucket):
-    batches = int(math.ceil(len(documents)/float(DEFAULT_BATCH_SIZE)))
-    for i in xrange(0, batches):
-        start = i * DEFAULT_BATCH_SIZE
-        try:
-            batch = {key: documents[key] for key in
-                     documents.keys()[start:start+25]}
-        except IndexError:
-            batch = {key: documents[key] for key in
-                     documents.keys()[start:]}
-        bucket.upsert_multi(batch)
+def store_in_cb(work_queue):
+    bkt = connect_to_cb()
+    get = work_queue.get
+    upsert_multi = bkt.upsert_multi
+    while not work_queue.empty():
+        docs = dict()
+        for _ in xrange(0, DEFAULT_BATCH_SIZE):
+            doc = get()
+            docs[doc[0]] = doc[1]
+        upsert_multi(docs)
 
 
 def connect_to_cb():
